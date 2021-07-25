7ce95c7087f265f0154de79f6d402bf10930b0d2
Justin Berger Howes <howes.j.j@gmail.com>
2021-07-26 07:47:09 +1000

    lab complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,26 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-    </div>
+    <h1>Movies Page</h1>
+      {movies.map(movie=>{
+        return(
+          <div key={movie.title}>
+            <React.Fragment>
+            <h2>{movie.title}</h2>
+            <h3>{movie.time}</h3>
+            <ul>
+              {movie.genres.map(genre=>{
+                return(
+                  <li key={genre}>{genre}</li>
+                )
+              })}
+            </ul>
+            </React.Fragment>
+          </div>
+        )
+        }
+        )}
+  </div>
   );
 };
 
